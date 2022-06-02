6f0118b3d064e3253b5c8126129e473a4aa612bf
Manisha Jha <manisha@diverseinfotech.com>
2022-06-02 17:47:02 +0000

    smoketest project commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("smoke.smoketest.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
